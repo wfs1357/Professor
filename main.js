2445ee9b23888bf1ab80c4b380e79fccf3e3143a
wesley <106327635+wfs1357@users.noreply.github.com>
2024-05-03 12:06:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const acrostico = document.getElementById('acrostico');
+const paragraphs = acrostico.getElementsByTagName('p');
+
+let currentColorIndex = 0;
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+function changeColor() {
+  for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = colors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+  }
+}
+
+setInterval(changeColor, 1000); // Altera a cor a cada segundo
 
